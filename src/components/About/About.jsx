f26fae08ed2ad920866cd13607132525f9817f54
Brenda Ferrufino <ferrufinob@gmail.com>
2021-06-20 11:07:38 -0400

    removed lorem

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -38,15 +38,15 @@ const About = () => {
             <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
               <div className="about-wrapper__info">
                 <p className="about-wrapper__info-text">
-                  {paragraphOne ||
-                    'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi neque, ipsa animi maiores repellendu distinctioaperiam earum dolor voluptatum consequatur blanditiis inventore debitis fuga numquam voluptate architecto itaque molestiae.'}
+                  {paragraphOne || 'Detail-oriented Full Stack Software Engineer'}
                 </p>
                 <p className="about-wrapper__info-text">
                   {paragraphTwo ||
-                    'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi neque, ipsa animi maiores repellendu distinctioaperiam earum dolor voluptatum consequatur blanditiis inventore debitis fuga numquam voluptate architecto itaque molestiae.'}
+                    'I am a passionate learner and problem solver, which sparked my curiosity in software engineering because it is continuously evolving with new technologies and new engineering trends. I value team work and believe that it creates more opportunities for creative ideas, better problem solving approaches and great learning opportunities.'}
                 </p>
                 <p className="about-wrapper__info-text">
-                  {paragraphThree || 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.'}
+                  {paragraphThree ||
+                    'Technologies Include: JavaScript, ReactJS, Rails, Redux, SQL, HTML, and CSS'}
                 </p>
                 {resume && (
                   <span className="d-flex mt-3">
